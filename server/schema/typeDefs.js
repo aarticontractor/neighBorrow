58460e8f19188df568a380272066a764f10f6274
lauriefish22 <120143917+lauriefish22@users.noreply.github.com>
2023-06-03 11:58:36 -0700

    added typedefs

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -1,16 +1,52 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Tool {
+  type Category {
+    _id: ID
+    name: String
+  }
+  type ProductType {
+    _id: ID
+    name: String
+    category: Category
+  }
+  type Product {
     _id: ID
     name: String
     description: String
+    image: String
     price: Float
+    productType: ProductType
+    category: Category
+  }
+  type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    password: String
+  }
+  type Auth {
+    token: ID
+    user: User
   }
 
   type Query {
-    getTool(id: ID!): Tool
-    getAllTools: [Tool]
+    product(id: ID!): Product
+    categories: [Category]
+    user: User
+    productType(id: ID!): ProductType
+  }
+
+  type Mutation {
+    addUser(
+      firstName: String!
+      lastName: String!
+      email: String!
+      password: String!
+    ):
+  ****Update user????***
+    
   }
 `;
 
