c34af3b9b35decb7b0e7f33f33a8e101fc28f8e8
aarticontractor <contaaru@gmail.com>
2023-06-04 16:43:00 -0700

    Converted user password to hashed password

--- server/models/User.js
+++ server/models/User.js
@@ -3,26 +3,52 @@ const { Schema } = mongoose;
 const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
-    firstName: {
-        type: String,
-        required: true,
-    },
-    lastName: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
+
+// Hash the password before saving the user
+userSchema.pre('save', async function (next) {
+  try {
+    if (!this.isModified('password')) {
+      return next();
+    }
+
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(this.password, saltRounds);
+    this.password = hashedPassword;
+
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+// Custom method to compare passwords
+userSchema.methods.comparePassword = async function (password) {
+  try {
+    return await bcrypt.compare(password, this.password);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 
 const User = mongoose.model('User', userSchema);
 
-module.exports = User; 
+module.exports = User;
