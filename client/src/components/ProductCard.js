1b129e5b4b90716ed2551cbb78b7cce005c25ea1
aarticontractor <contaaru@gmail.com>
2023-06-06 10:33:28 -0700

    added animeJS for page animations

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Card, CardActionArea, CardContent, CardMedia, Typography, Button } from '@material-ui/core';
+import anime from 'animejs';
 
 const ProductCard = ({ product, onProductClick }) => {
+    const nameRef = useRef(null);
+    const priceRef = useRef(null);
+
+    useEffect(() => {
+        anime({
+            targets: [nameRef.current, priceRef.current],
+            scale: [0.1, 1],
+            opacity: [0, 1],
+            delay: anime.stagger(100),
+            easing: 'spring(1, 80, 10, 0)'
+        });
+    }, []);
+
     return (
         <Card>
             <CardActionArea onClick={() => onProductClick(product)}>
@@ -13,10 +27,10 @@ const ProductCard = ({ product, onProductClick }) => {
                     title={product.name}
                 />
                 <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
+                    <Typography gutterBottom variant="h5" component="div" ref={nameRef}>
                         {product.name}
                     </Typography>
-                    <Typography variant="body2" color="text.secondary">
+                    <Typography variant="body2" color="text.secondary" ref={priceRef}>
                         ${product.price}
                     </Typography>
                 </CardContent>
