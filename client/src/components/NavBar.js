2eb75dfbbcb4c58687c823df2baf3d0f61e6c4d4
aarticontractor <contaaru@gmail.com>
2023-06-05 19:42:36 -0700

    Dummy html for NavBar & SignupLogin

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <nav>
+            <h2>NavBar</h2>
+            {/* Here you can add your navigation links */}
+        </nav>
+    );
+}
+
+export default NavBar;
---
+++ client/src/pages/SignupLogin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SignupLogin = () => {
+    return (
+        <div>
+            <h1>Signin/Login Page</h1>
+            {/* Here you can add your signin/login form */}
+        </div>
+    );
+}
+
+export default SignupLogin;
