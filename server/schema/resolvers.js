41e251d8a243a6c62bcb43a248e0b48591744df6
lauriefish22 <120143917+lauriefish22@users.noreply.github.com>
2023-06-06 18:02:17 -0700

    added to resolvers

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -20,6 +20,49 @@ const resolvers = {
         getProductByID: async (_, { productId }) => {
             return await Product.findById(productId).populate('user').populate('category');
         },
+        order: async (parent, { _id }, context) => {
+            if (context.user) {
+                const user = await User.findById(context.user._id).populate({
+                    path: 'orders.products',
+                    populate: 'category'
+                });
+                return user.orders.id(_id);
+            }
+            throw new AuthenticationError('Not logged in');
+        },
+        checkout: async (parent, args, context) => {
+            const url = new URL(context.headers.referer).origin
+            const order = newOrder({ products: args.products });
+            const { products } = await order.populate('products').execPopulate();
+
+            const line_items = [];
+
+            for (let i = 0; i < products.length; i++) {
+                const product = await stripe.products.create({
+                    name: products[i].name,
+                    description: products[i].description,
+                    images: []
+                });
+                const price = await stripe.prices.create({
+                    product: product.id,
+                    unit_amount: products[i].price * 100,
+                    currency: 'usd',
+                });
+                line_items.push({
+                    price: price.id,
+                    quantity: 1,
+                });
+                const session = await stripe.checkout.sessions.create({
+                    payment_method_types: ['card'],
+                    line_items,
+                    mode: 'payment',
+                    success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+                    cancel_url: `${url}`,
+                });
+                return { session: session.id };
+            }
+        }
+
     },
     Mutation: {
         addUser: async (_, args) => {
