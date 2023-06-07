59ac74af1a0c837d9a223c7f1db5f4d4246bf472
lauriefish22 <120143917+lauriefish22@users.noreply.github.com>
2023-06-06 20:29:52 -0700

    added reducers

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,3 +1,4 @@
+import { useReducer } from 'react';
 import {
     UPDATE_PRODUCTS,
     ADD_TO_CART,
@@ -15,8 +16,59 @@ const initialState = {
     categories: [],
     currentCategory: '',
 };
-export const reducer = (state = initialState, action) => {
+export const reducer = (state, action) => {
     switch (action.type) {
-
+        case UPDATE_PRODUCTS:
+            return {
+                ...state,
+                products: [...action.products],
+            };
+        case ADD_MULTIPLE_TO_CART:
+            return {
+                ...state,
+                cart: [...state.cart, ...action.products],
+            };
+        case REMOVE_FROM_CART:
+            let newState = state.cart.filter((product) => {
+                return product.id !== action._id;
+            });
+            return {
+                ...state,
+                cartOpen: newState.length > 0,
+                cart: newState,
+            };
+        case UPDATE_CART_QUANTITY:
+            return {
+                ...state,
+                cartOpen: true,
+                cart: state.cart.map((product) => {
+                    if (action._id === product._id) {
+                        product.purchaseQuantity = action.purchaseQuantity;
+                    }
+                    return product;
+                }),
+            };
+        case CLEAR_CART:
+            return {
+                ...state,
+                cartOpen: false,
+                cart: [],
+            };
+        case ADD_TO_CART:
+            return {
+                ...state,
+                cartOpen: true,
+                cart: [...state.cart, action.product],
+            };
+        case TOGGLE_CART:
+            return {
+                ...state,
+                cartOpen: !state.cartOpen,
+            };
+        default:
+            return state;
     }
+};
+export function useProductReducer(initialState) {
+    return useReducer(reducer, initialState);
 }
