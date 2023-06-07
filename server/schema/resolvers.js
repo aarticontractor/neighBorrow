8515f65685ff03a563f9ffa799a8d20139559159
lauriefish22 <120143917+lauriefish22@users.noreply.github.com>
2023-06-06 18:14:29 -0700

    resolvers DONE

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -93,6 +93,22 @@ const resolvers = {
 
             return { category };
         },
+        updateUser: async (parent, args, context) => {
+            if (context.user) {
+                return await User.findByIdAndUpdate(context.user._id, args, {
+                    new: true,
+                });
+            }
+            throw new AuthenticationError('Not logged in');
+        },
+        updateProduct: async (parent, { _id, quantity }) => {
+            const decrement = Math.abs(quantity) * -1;
+            return await Product.findByIdAndUpdate(
+                _id,
+                { $inc: { quantity: decrement } },
+                { new: true }
+            );
+        },
 
         login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
