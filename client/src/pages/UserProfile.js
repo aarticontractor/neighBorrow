050ce076b5dad4131851cc16bb69d9e622dee51d
Berkeleycodingmomma <floridabelle2021@protonmail.com>
2023-06-07 20:13:16 -0600

    Added back in avatar code and cleaned up commented code

--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Box, Grid, Container } from '@material-ui/core';
+import { Avatar, Button, Box, Grid, Container, IconButton } from '@material-ui/core';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
 import Auth from '../utils/auth';
 import { useQuery } from '@apollo/client';
 import { useStoreContext } from '../utils/globalState';
@@ -17,6 +18,7 @@ const UserProfile = () => {
     firstName: userObj.firstName,
     lastName: userObj.lastName,
     userId: userObj._id,
+    avatar: ''
   });
   const [state ] = useStoreContext();
   const { loading, error,  data } = useQuery(GET_ALL_PRODUCTS);
@@ -32,6 +34,29 @@ const UserProfile = () => {
     }
     filterByUser();
   }, [products, data]);
+
+  const handleAvatarUpload = async (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append('avatar', file);
+
+    try {
+      const response = await fetch('/upload-avatar', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      setUser({ ...user, avatar: data.path });
+
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
 
   if (loading) return 'Loading...';
   if (error) return `Error! ${error.message}`;
@@ -54,7 +79,27 @@ const UserProfile = () => {
           >
             Profile
           </Typography>
-          
+          <Stack
+            sx={{ pt: 4 }}
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            spacing={2}
+          >
+            <Avatar alt="User Avatar" src={user.avatar} style={{ width: 100, height: 100 }} />
+            <input
+              accept="image/*"
+              id="icon-button-file"
+              type="file"
+              style={{ display: 'none' }}
+              onChange={handleAvatarUpload}
+            />
+            <label htmlFor="icon-button-file">
+              <IconButton color="primary" aria-label="upload picture" component="span">
+              <PhotoCamera />
+              </IconButton>
+            </label>
+          </Stack>
           <Stack
             sx={{ pt: 4 }}
             direction="column"
@@ -131,178 +176,7 @@ const UserProfile = () => {
               ))}
             </Grid>
         </Container>
-      
     </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // <Container>
-
-    //   <Grid 
-    //     container
-    //     direction="column"
-    //     justifyContent="center"
-    //     alignItems="center"
-    //     item 
-    //     xs={12}
-    //     style={{marginTop: "2em"}}
-    //     >
-    //      <Typography variant="h4">User Profile</Typography>
-    //  </Grid>
-    //   <Paper style={{padding: "1em", marginTop: "2em"}}>
-    //   <Grid 
-    //     container
-    //     item
-    //     // xs={1}
-    //     direction="column"
-    //     justifyContent="center"
-    //     alignItems="center"
-    //     style={{marginTop: "2em"}}
-    //   >
-    //     <Grid 
-    //       container 
-    //       justifyContent="center"
-    //       alignItems="center" 
-    //       item
-    //       columns={16}
-    //       // xs={1}
-    //       >
-    //       <Grid 
-    //         container 
-    //         item
-    //         xs={4} 
-    //         direction="column"
-    //         justifyContent="center"
-    //         alignItems="center"
-    //         alignContent='center' 
-    //         >
-    //         <Grid 
-    //           container
-    //           item
-    //           direction="column"
-    //           justifyContent="center"
-    //           alignItems="center"
-    //           alignContent='center'
-    //           spacing={2}
-    //           // xs={1}
-    //         >
-    //           <Avatar alt="User Avatar" src={user.avatar} style={{ width: 100, height: 100 }} />
-    //           <input
-    //             accept="image/*"
-    //             id="icon-button-file"
-    //             type="file"
-    //             style={{ display: 'none' }}
-    //             onChange={handleAvatarUpload}
-    //           />
-    //           <label htmlFor="icon-button-file">
-    //             <IconButton color="primary" aria-label="upload picture" component="span">
-    //               <PhotoCamera />
-    //               </IconButton>
-    //           </label>
-    //         </Grid>
-    //       </Grid>
-    //       <Grid 
-    //         container direction="column"
-    //         justifyContent="center"
-    //         item 
-    //         xs={6}
-    //       >
-    //         <Typography variant="h4">{user.name}</Typography>
-    //         <Typography>User ID: {user.userId}</Typography>
-    //         <Typography>Items Selling: {user.selling.length}</Typography>
-    //         <Typography>Active Selling: {user.activeSelling.length}</Typography>
-    //         <Typography>Balance: ${user.balance}</Typography>
-    //         <Typography>Total Sales: ${user.totalSales}</Typography>
-    //       </Grid>
-    //     </Grid>
-    //   </Grid>
-    //   </Paper>
-    //   <Grid 
-    //     container
-    //     direction="row"
-    //     justifyContent="center"
-    //     alignItems="center"
-    //     item 
-    //     xs={12} 
-    //     style={{marginTop: "2em"}}>
-    //      <Button variant="contained" color="primary" onClick={handleEditProfile} style={{marginRight: "2em"}}>
-    //        Edit Profile
-    //      </Button>
-    //      <Button component={Link} to="/public-profile" variant="contained" color="secondary">
-    //        View Public Profile
-    //      </Button>
-    //    </Grid>
-    // </Container>
-
-    // <Grid container direction="column" alignItems="center" spacing={2} >
-    //   <Grid item xs={12}>
-    //     <Typography variant="h4">User Profile</Typography>
-    //   </Grid>
-    //     {/* <Paper > */}
-    //   <Grid item xs={12}>
-    //       {/* <Box p={5}> */}
-    //         <Grid container  columns={30} style={{border: "solid 2px red"}}>
-    //           <Grid item xs={15} sm={15} style={{border: "solid 2px blue"}}>
-    //             <Avatar alt="User Avatar" src={user.avatar} />
-    //             <input
-    //               accept="image/*"
-    //               id="icon-button-file"
-    //               type="file"
-    //               style={{ display: 'none' }}
-    //               onChange={handleAvatarUpload}
-    //             />
-    //             <label htmlFor="icon-button-file">
-    //               <IconButton color="primary" aria-label="upload picture" component="span">
-    //                 <PhotoCamera />
-    //               </IconButton>
-    //             </label>
-    //           </Grid>
-    //           <Grid item xs={15} sm={15} style={{border: "solid 2px blue"}}>
-    //             <Typography variant="h5">{user.name}</Typography>
-    //             <Typography>User ID: {user.userId}</Typography>
-    //             <Typography>Items Selling: {user.selling.length}</Typography>
-    //             <Typography>Active Selling: {user.activeSelling.length}</Typography>
-    //             <Typography>Balance: ${user.balance}</Typography>
-    //             <Typography>Total Sales: ${user.totalSales}</Typography>
-    //           </Grid>
-    //         </Grid>
-    //       {/* </Box> */}
-    //   </Grid>
-    //     {/* </Paper> */}
-    //   <Grid item xs={12}>
-    //     <Button variant="contained" color="primary" onClick={handleEditProfile}>
-    //       Edit Profile
-    //     </Button>
-    //   </Grid>
-    //   <Grid item xs={12}>
-    //     <Button component={Link} to="/public-profile" variant="contained" color="secondary">
-    //       View Public Profile
-    //     </Button>
-    //   </Grid>
-    // </Grid>
   );
 };
 
